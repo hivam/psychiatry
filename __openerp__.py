c690a2e6aa65d078e70346b5270edb56fb491284
hivam <quipus.total@gmail.com>
2016-10-01 11:00:51 -0500

    whoqolbref

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'Psychiatry',
+    'version' : '1.0',
+    'summary': 'Psychiatric evaluation',
+    'description': """
+This module allows
+=================================================================
+
+""",
+    'category' : 'Health',
+    'author' : 'Hector Ivan Valencia Muñoz',
+    'website': 'http://www.odoo-co.blogspot.com',
+    'license': 'AGPL-3',
+    'depends' : [],
+    'data' : [
+                'views/psychiatry_view.xml',
+              ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ models/psychiatry.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class psychiatry_whoqolbref_answer(osv.osv):
+    _name = 'psychiatry.whoqolbref.answer'
+    _columns = {
+        'category': fields.selection([('G', 'General'), ('F', 'Salud física'),
+                                      ('P', 'Psicológica'), ('R', 'Relaciones interpersonales'),
+                                      ('E', 'Entorno')], 'Categoría'),
+        'answer': fields.char('Respuesta', size=25),
+        'measure': fields.integer('Valor', size=1),
+        }
+
+    def name_get(self, cr, uid, ids, context={}):
+        if not len(ids):
+            return []
+        rec_name = 'answer'
+        res = [(r['id'], r[rec_name][1])
+               for r in self.read(cr, uid, ids, [rec_name], context)]
+        return res
+
+co_presupuesto()
