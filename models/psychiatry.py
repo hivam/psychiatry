4104cf9292aad4b064d05d8ac50be2f8a60d8704
hivam <quipus.total@gmail.com>
2016-10-07 16:31:26 -0500

    whoqolbref fill answer_scale

--- models/psychiatry.py
+++ models/psychiatry.py
@@ -9,29 +9,19 @@ import logging
 # from openerp.tools import email_split
 # from math import floor
 logger = logging.getLogger(__name__)
-
-# from openerp.osv import fields, osv
-# from openerp.tools.translate import _
 # import time
 
-# class psychiatry_whoqolbref_answer(osv.osv):
 class Psychiatry_Whoqolbref_Answer(models.Model):
     _name = 'psychiatry.whoqolbref.answer'
-    # _rec_name = 'answer'
-    # _columns = {
+
     name= fields.Char(string=u'Respuesta', size=25)
     answer_scale= fields.Selection([('A', 'A'), ('B', 'B'), ('C', 'C'),
                                     ('D', 'D'), ('E', 'E'), ('F', 'F')], string=u'Escala')
     measure= fields.Integer(string=u'Valor', size=1)
-        # }
 
-# psychiatry_whoqolbref_answer()
-
-# class psychiatry_whoqolbref_question(osv.osv):
 class Psychiatry_Whoqolbref_Question(models.Model):
     _name = 'psychiatry.whoqolbref.question'
-    # _rec_name = 'question'
-    # _columns = {
+
     name= fields.Char(string=u'Pregunta', size=150)
     category= fields.Selection([('G', 'General'), ('F', 'Salud física'),
                                 ('P', 'Psicológica'), ('R', 'Relaciones interpersonales'),
@@ -39,23 +29,12 @@ class Psychiatry_Whoqolbref_Question(models.Model):
     answer_scale= fields.Selection([('A', 'A'), ('B', 'B'), ('C', 'C'),
                                     ('D', 'D'), ('E', 'E'), ('F', 'F')], string=u'Escala')
     active= fields.Boolean('Active', default=True)
-        # }
 
-    # _defaults = {
-    #     'active': 1,
-    # }
-
-# psychiatry_whoqolbref_question()
-
-# class psychiatry_whoqolbref_evaluation(osv.osv):
 class psychiatry_whoqolbref_evaluation(models.Model):
     _name = 'psychiatry.whoqolbref.evaluation'
-    # _rec_name = 'date'
-    # _columns = {
-    # name = fields.Char(string=u'Referencia')
+
     date= fields.Date()
     question_ids= fields.One2many('psychiatry.whoqolbref.questions', 'evaluation_id')
-        # }
 
     @api.onchange('date')
     def _onchange_date(self):
@@ -68,26 +47,12 @@ class psychiatry_whoqolbref_evaluation(models.Model):
             logger.info('##########################################')
         self.question_ids = question_fill
 
-    # def _get_question_ids(self, cr, uid, context):
-    #     ids = self.pool.get('psychiatry.whoqolbref.question').search(cr, uid, [( 'active',  '=', 1)], context=context)
-    #     return ids
-
-    # _defaults = {
-    # 'date': lambda *a: time.strftime('%Y-%m-%d'),
-    # 'question_ids': _get_question_ids,
-    # }
-
-# psychiatry_whoqolbref_evaluation()
-
-# class psychiatry_whoqolbref_questions(osv.osv):
 class Psychiatry_Whoqolbref_Questions(models.Model):
     _name = "psychiatry.whoqolbref.questions"
-    # _rec_name = 'evaluation_id'
-    # _columns = {
-    # name = fields.Char(string=u'Referencia'),
+
     evaluation_id= fields.Many2one('psychiatry.whoqolbref.evaluation', ondelete='cascade')
-    question_id= fields.Many2one('psychiatry.whoqolbref.question')
-        # 'answer_scale': fields.related('question_id', 'answer_scale', string="Escala", type="char", store=True),
+    question_id= fields.Many2one('psychiatry.whoqolbref.question', string=u'Pregunta')
+    answer_scale= fields.Char(related='question_id.answer_scale', store=True)
         # 'answer_id': fields.many2one('psychiatry.whoqolbref.answer', 'Respuesta'),
         # 'answer_measure': fields.related('answer_id', 'measure', string="Valor", type="integer", store=True),
     # }
