5f5afabfe9d8dedb3c91a34704c6394ee0c3eba5
hivam <quipus.total@gmail.com>
2016-10-08 15:07:45 -0500

    test scl90r

--- models/psychiatry.py
+++ models/psychiatry.py
@@ -16,7 +16,8 @@ class Psychiatry_Whoqolbref_Answer(models.Model):
 
     name= fields.Char(string=u'Respuesta', size=25)
     answer_scale= fields.Selection([('A', 'A'), ('B', 'B'), ('C', 'C'),
-                                    ('D', 'D'), ('E', 'E'), ('F', 'F')], string=u'Escala')
+                                    ('D', 'D'), ('E', 'E'), ('F', 'F'),
+                                    ('G', 'G')], string=u'Escala')
     measure= fields.Integer(string=u'Valor', size=1)
 
 class Psychiatry_Whoqolbref_Question(models.Model):
@@ -42,9 +43,9 @@ class psychiatry_whoqolbref_evaluation(models.Model):
         question_fill=[]
         for record in questions_pool.search([('active','=', 1)]):
             question_fill.append([0, 0,{'question_id': record.id}])
-            logger.info('##########################################')
-            logger.info(question_fill)
-            logger.info('##########################################')
+            # logger.info('##########################################')
+            # logger.info(question_fill)
+            # logger.info('##########################################')
         self.question_ids = question_fill
 
 class Psychiatry_Whoqolbref_Questions(models.Model):
@@ -52,6 +53,43 @@ class Psychiatry_Whoqolbref_Questions(models.Model):
 
     evaluation_id= fields.Many2one('psychiatry.whoqolbref.evaluation', ondelete='cascade')
     question_id= fields.Many2one('psychiatry.whoqolbref.question', string=u'Pregunta')
+    answer_scale= fields.Selection(related='question_id.answer_scale', store=True)
+    answer_id= fields.Many2one('psychiatry.whoqolbref.answer', string=u'Respuesta')
+    answer_measure= fields.Integer(related='answer_id.measure', store=True, string=u'Valor')
+
+#################################################################################################################
+
+class Psychiatry_Scl90r_Question(models.Model):
+    _name = 'psychiatry.scl90r.question'
+
+    name= fields.Char(string=u'Pregunta', size=150)
+    category= fields.Selection([('SOM', 'Somatizaciones'), ('OBS', 'Obsesiones y compulsiones'),
+                                ('SI', 'Sensitividad interpersonal'), ('DEP', 'Depresión'),
+                                ('ANS', 'Ansiedad'), ('HOS', 'Hostilidad'),
+                                ('FOB', 'Ansiedad fóbica'), ('PAR', 'Ideación paranoide'),
+                                ('PSIC', 'Psicoticismo'), ('IA', 'Ítems adicionales')], string=u'Dimensión')
+    answer_scale= fields.Selection([('G', 'G')], string=u'Escala')
+    active= fields.Boolean('Active', default=True)
+
+class psychiatry_Scl90r_evaluation(models.Model):
+    _name = 'psychiatry.scl90r.evaluation'
+
+    date= fields.Date()
+    question_ids= fields.One2many('psychiatry.scl90r.questions', 'evaluation_id')
+
+    @api.onchange('date')
+    def _onchange_date(self):
+        questions_pool = self.env['psychiatry.scl90r.question']
+        question_fill=[]
+        for record in questions_pool.search([('active','=', 1)]):
+            question_fill.append([0, 0,{'question_id': record.id}])
+        self.question_ids = question_fill
+
+class Psychiatry_Scl90r_Questions(models.Model):
+    _name = "psychiatry.scl90r.questions"
+
+    evaluation_id= fields.Many2one('psychiatry.scl90r.evaluation', ondelete='cascade')
+    question_id= fields.Many2one('psychiatry.scl90r.question', string=u'Pregunta')
     answer_scale= fields.Selection(related='question_id.answer_scale', store=True)
     answer_id= fields.Many2one('psychiatry.whoqolbref.answer', string=u'Respuesta')
     answer_measure= fields.Integer(related='answer_id.measure', store=True, string=u'Valor')
