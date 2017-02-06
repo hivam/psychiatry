8a42b5e19a96ac18ebd1c4579df35b427459c164
hivam <quipus.total@gmail.com>
2017-02-05 21:32:37 -0500

    Agregando vista tipo search - Pacientes

--- __openerp__.py
+++ __openerp__.py
@@ -18,7 +18,7 @@
                 'security/ir.model.access.csv',
                 'views/psychiatry_view.xml',
                 'data/psychiatry_sequences.xml',
-                # 'data/psychiatry.diseases.csv'
+                'data/psychiatry.diseases.csv'
               ],
     'installable': True,
     'auto_install': False,
